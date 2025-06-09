2f351a4b4bfdf19d6a3342db1316be1e851a9f36
Noah Jefferson Baumann <noah.jefferson.baumann.1@hu-berlin.de>
2025-06-09 14:06:29 +0100

    setup backend to use aura db instead of server version

--- web/backend/neo4j.js
+++ web/backend/neo4j.js
@@ -1,7 +1,7 @@
 // backend/neo4j.js
 require('dotenv').config();
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
+const password = process.env.NEO4J_AURA_PASSWORD; // NEO4J_AURA_PASSWORD or DB_PASSWORD
+const host = process.env.NEO4J_AURA_URI; // NEO4J_AURA_URI or DB_HOST
 
 const neo4j = require('neo4j-driver');
 
